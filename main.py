1ee24efbc741f1dd8c986de0217d71bfb09b67f7
LawrenceJ1 <lawrencejuottawa@gmail.com>
2021-12-31 18:18:23 -0500

    add comments

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as soup
 from time import sleep
 
+#initializing the webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.websudoku.com/")
 driver.switch_to.frame(0)
+
+#getting puzzle info
 page = soup(driver.page_source, features="html.parser")
-print(page)
+print(page.find("table", {"id":"puzzle_grid"}))
 driver.close()
