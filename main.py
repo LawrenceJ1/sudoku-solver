aae875f9f35b4ea060aba63782c9c11849e0202c
LawrenceJ1 <lawrencejuottawa@gmail.com>
2021-12-31 18:16:43 -0500

    get the source for the sudoku

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as soup
+from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://www.websudoku.com/")
+driver.switch_to.frame(0)
+page = soup(driver.page_source, features="html.parser")
+print(page)
+driver.close()
