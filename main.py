e63aae475e610b3218ea07e89296636514ca7a3a
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-02-03 15:34:06 -0500

    add margin and side lengths

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ class Sudoku(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
-        self.width = 800
-        self.height = 600
+        self.margin = 20
+        self.side = 60 
+        self.width = self.height = self.margin * 2 + self.side * 9 
         self.createWidgets()
         
     def createWidgets(self):
@@ -17,8 +18,8 @@ class Sudoku(tk.Frame):
     def _drawGrid(self):
         for i in range(10):
             color = "blue" if i % 3 == 0 else "grey"
-            self.canvas.create_line(i*(self.width/9), 0, i*(self.width/9), self.height, fill=color)
-            self.canvas.create_line(0, i*(self.height/9), self.width, i*(self.height/9), fill=color)
+            self.canvas.create_line(self.margin+i*(self.side), self.margin, self.margin+i*(self.side), self.height-self.margin, fill=color)
+            self.canvas.create_line(self.margin, self.margin+i*(self.side), self.width-self.margin, self.margin+i*(self.side), fill=color)
         
 app = Sudoku()
 app.master.title("Sudoku")
